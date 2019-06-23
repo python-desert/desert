366b1e84c4b9c94c3bffdb136c2a7e386114a05b
desert <desert@cordaz.com>
2019-06-23 04:07:59 +0000

    Add tests

--- tests/test_marshmallow_attrs.py
+++ tests/test_marshmallow_attrs.py
@@ -1,5 +1,30 @@
+import attr
+import marshmallow
+import pytest
+
+
 import marshmallow_attrs
 
 
 def test_init():
     pass
+
+
+def test_simple():
+    @marshmallow_attrs.dataclass
+    class A:
+        x: int = attr.ib()
+
+    data, errors = marshmallow_attrs.class_schema(A)(strict=True).load(data={"x": 5})
+    assert not errors
+    assert data == A(x=5)
+
+
+def test_validation():
+    @marshmallow_attrs.dataclass
+    class A:
+        x: int = attr.ib()
+
+    schema = marshmallow_attrs.class_schema(A)(strict=True)
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        schema.load({"y": 5})
