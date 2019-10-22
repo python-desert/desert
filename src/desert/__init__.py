102d5c77a9cee01f48be3b9888d999b76b645b3c
desert <desert@cordaz.com>
2019-10-22 23:45:25 +0000

    Remove **kw

--- src/desert/__init__.py
+++ src/desert/__init__.py
@@ -1,11 +1,16 @@
 import desert._make
 
 
-def schema(cls, many=False, **kw):
-    """Build a schema for the class."""
-    return desert._make.class_schema(cls, **kw)(many=many)
+def schema(cls, many=False):
+    """Build a schema for the class.
+    """
+    return desert._make.class_schema(cls)(many=many)
 
 
-def schema_class(cls, **kw):
-    """Build a schema class for the class."""
-    return desert._make.class_schema(cls, **kw)
+def schema_class(cls):
+    """Build a schema class for the class.
+
+
+
+    """
+    return desert._make.class_schema(cls)
