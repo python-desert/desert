c05f0f0948c0765f75fc29306353f374ef0e7c76
desert <desert@cordaz.com>
2019-10-24 06:54:01 +0000

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = "desert"
 year = "2019"
 author = "Desert contributors"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.1.3"
+version = release = "0.1.4"
 
 pygments_style = "trac"
 templates_path = ["."]
